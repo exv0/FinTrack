3f95968399791c8bbe3404b56b13ebe0b6ead204
blissexodia <binnoley@gmail.com>
2026-06-28 11:59:06 +0545

    login additions

--- client/app/(auth)/login/page.jsx
+++ client/app/(auth)/login/page.jsx
@@ -111,7 +111,7 @@ export default function LoginPage() {
 
       <div className={styles.orDivider}><span>or</span></div>
 
-      <button className={styles.btnGhost} type="button">
+      <button className={styles.btnGhost} type="button" disabled title="Google sign-in is not available yet">
         <GoogleIcon />
         Continue with Google
       </button>
